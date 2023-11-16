2217aca67bb62d381ba39cfb0f627cae4ca48f22
maruf <maruf112022@gmail.com>
2023-11-17 00:35:58 +0600

    flowbite setup done

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,11 @@
+import { Button } from 'flowbite-react';
 import React from 'react';
 
 const Home = () => {
     return (
         <div>
             <h1 className='text-xl'>
-                This is home
+                This is home <Button>Click me</Button>
             </h1>
         </div>
     );
