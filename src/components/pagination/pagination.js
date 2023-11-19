17bb06adaee367d7ea5492850dc2b4845559ba88
maruf <maruf112022@gmail.com>
2023-11-19 12:35:55 +0600

    pagination done

---
+++ src/components/pagination/pagination.js
@@ -0,0 +1,11 @@
+import { Pagination } from 'flowbite-react';
+export function PaginationUi({ currentPage, setCurrentPage }) {
+    const onPageChange = (page: number) => setCurrentPage(page);
+    // console.log({ currentPage });
+
+    return (
+        <div className="flex overflow-x-auto sm:justify-center">
+            <Pagination currentPage={currentPage} totalPages={100} onPageChange={onPageChange} showIcons />
+        </div>
+    );
+}
--- src/redux/features/users/usersApi.js
+++ src/redux/features/users/usersApi.js
@@ -2,7 +2,7 @@ import { api } from "../api/apiSlice";
 const usersApi = api.injectEndpoints({
     endpoints: (builder) => ({
         getUsers: builder.query({
-            query: () => `users`,
+            query: (page) => `users?page=${page}`,
         }),
     })
 });
